c7f1f6b93622e2db1879d4cf54f0b613483157a5
revathy3 <revathy@steyp.com>
2023-06-02 12:52:02 +0530

    practices

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Counter(props) {
+  return (
+    <div>
+      <h1>
+        {props.title}
+        {props.count}
+      </h1>
+    </div>
+  );
+}
+
+export default Counter;
