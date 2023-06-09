8ca70b51d75d4239f335dae31b6419672b441d96
revathy3 <revathy@steyp.com>
2023-06-09 19:28:49 +0530

    counting condition

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useState } from "react";
 
 function Counter() {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Hello, I am components</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <h1>Hello, I am components :{count}</h1>
     </div>
   );
 }
